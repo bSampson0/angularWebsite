633b288a9a0080d3371cf7772b33fa387a8c86e6
Bryce S <bnicerrr@gmail.com>
2018-01-07 15:09:09 -0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "[AIzaSyAx9Kl5U9YpYg5cqyroMuMOb3jAdKozk34]",
-    authDomain: "[marcowebsite-6cdf9.firebaseapp.com]",
-    databaseURL: "[https://marcowebsite-6cdf9.firebaseio.com]",
-    projectId: "[marcowebsite-6cdf9]",
+    apiKey: "[ ]",
+    authDomain: "[ ]",
+    databaseURL: "[ ]",
+    projectId: "[ ]",
     storageBucket: "",
-    messagingSenderId: "[288493631194]"
+    messagingSenderId: "[ ]"
   }
 };
